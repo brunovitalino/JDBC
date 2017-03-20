39b65485406a884b21d771589b03315fe3dcec05
Bruno Vitalino <brunovitalino@hotmail.com>
2017-03-20 03:04:44 -0300

    Corrigido o erro que fazia a classe Main não ser reconhecida durante a compilação.

--- JDBC/src/view/Main.java
+++ JDBC/src/view/Main.java
@@ -7,7 +7,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		String nome = "Mvc";
+		String nome = "Alcantara";
 		COperacoesSQL operacoes = new COperacoesSQL();
 		operacoes.inserir(nome);
 	}
