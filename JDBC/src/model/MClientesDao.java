f2db3c02b2434ac51b4bb04a4a8003672b00dffe
Bruno Vitalino <brunovitalino@hotmail.com>
2017-03-21 23:20:32 -0300

    Adicionada a função pesquisa.

--- JDBC/src/model/MClientesDao.java
+++ JDBC/src/model/MClientesDao.java
@@ -13,52 +13,104 @@ public class MClientesDao
 	private Connection conexao;
 	private PreparedStatement stmt;
 	
-	public MClientesDao() {
+	public MClientesDao()
+	{
 		this.conexao = new MConnectionFactory().getConnection();
 	}
 	
 	//Nossa camada de controle usara para fechar a conexao
-	public void close() {
-		try {
+	public void close()
+	{
+		try
+		{
 			this.conexao.close();
 			System.out.println("Database desconectado.\n");
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			System.out.println("Não há conexão à ser fechada.\n");
+		}
+	}	
+	//PESQUISAR
+	public List<MCliente> pesquisar(String nome)
+	{
+		String comandoSQL = "";
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		MCliente cliente = null;
+		List<MCliente> clientes = new ArrayList<MCliente>();
+		
+		comandoSQL =	"SELECT * FROM clientes " +
+						"WHERE nome like ?";		
+		try
+		{
+			pst = conexao.prepareStatement(comandoSQL);
+			pst.setString(1, "%"+nome+"%");
+			rs = pst.executeQuery();
+			while (rs.next())
+			{
+				cliente = new MCliente();
+				cliente.setId(rs.getShort("id"));
+				cliente.setNome(rs.getString("nome"));
+				clientes.add(cliente);
+			}
+			pst.close();
+			rs.close();
+			return clientes;
+		} catch (SQLException e)
+		{
+			return null;
 		}
 	}
 	
-	public boolean inserir(String nome) {
+	//CADASTRAR
+	public boolean inserir(String nome)
+	{
 		String comandoSQL;
 		PreparedStatement pst = null;
 		
-		comandoSQL =	"insert into Clientes(nome)" +
-						"values (?);";
-		try {
+		comandoSQL =	"INSERT INTO Clientes(nome) " +
+						"VALUES (?);";
+		try
+		{
 			pst = conexao.prepareStatement(comandoSQL);
 			pst.setString(1, nome);
 			pst.execute();
 			pst.close();
 			return true;
 		}
-		catch (SQLException e) {
+		catch (SQLException e)
+		{
 			return false;
 		}
 	}
-	
-	public List<MCliente> listar() {
+	//ALTERAR
+	public void alterar()
+	{
+		
+	}
+	//REMOVER
+	public void remover()
+	{
+		
+	}
+	//LISTAR
+	public List<MCliente> listar()
+	{
 		String comandoSql = "";
 		Statement st = null;
 		ResultSet rs = null;
 		MCliente cliente = null;
 		List<MCliente> clientes = new ArrayList<MCliente>();
 
-		System.out.println("PESQUISANDO NO BANCO");
-		comandoSql = "select * from Clientes";
+		comandoSql = "SELECT * FROM Clientes";
 		
-		try {
+		try
+		{
 			st = conexao.createStatement();
 			rs = st.executeQuery(comandoSql);
-			while (rs.next()) {
+			while (rs.next())
+			{
 				cliente = new MCliente();
 				cliente.setId(rs.getShort("id"));
 				cliente.setNome(rs.getString("nome"));
@@ -67,7 +119,9 @@ public class MClientesDao
 			st.close();
 			rs.close();
 			return clientes;
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			return null;
 		}
 		finally
